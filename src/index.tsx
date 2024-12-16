ecd6bde1259152e7210a667cc3319b00bbaaf4f9
divan <divan21501@gmail.com>
2024-12-16 18:43:18 +0700

    prepared build branch

--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import 'react-app-polyfill/ie9';
-import 'react-app-polyfill/stable';
-import { createRoot } from 'react-dom/client';
-
-// import './styles/main.scss';
-import { App } from './App';
-
-const root = createRoot(document.getElementById('root')!);
-root.render(<App />);
